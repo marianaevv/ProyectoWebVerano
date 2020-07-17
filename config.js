8235a4c4821119b9eaeb51bebd1bb5b002db1091
marianaevv <mariana2v@hotmail.com>
2020-07-17 11:30:03 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-exports.PORT = process.env.PORT || '8080';
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb+srv://admin:admin1996@cluster0.vmukv.mongodb.net/viaergosumdb?retryWrites=true&w=majority'
-exports.TOKEN_ADMI = process.env.TOKEN_ADMI || 'TokenAdmi';
-exports.SECRET_TOKEN = process.env.SECRET_TOKEN || 'secretToken'
+
