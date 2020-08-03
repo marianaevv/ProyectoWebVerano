1990da52694204489ff9f4228f15a7a7dcc1ca75
Marianavv <mariana2v@hotmail.com>
2020-08-03 10:34:42 -0500

    Final changes

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -8,20 +8,3 @@ module.exports = (req, res) => {
         res.redirect('/auth/login')
     })
 }
-/*
-const User = require('../models/userModel')
-
-module.exports = (req, res) => {
-    //Creating new post
-    User.create(req.body,(error, user)=>{
-        if(error){
-            console.log(error);
-        }else{
-            console.log("Success",user);
-            res.redirect("login");
-            res.status(201).json();
-            
-        }
-    });
-    
-}*/
